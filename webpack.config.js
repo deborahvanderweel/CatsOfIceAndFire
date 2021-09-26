960aae130090a1f55de351b319e2532e04dbc506
deborahvanderweel <deborah.vanderweel@coolblue.nl>
2021-09-26 22:56:41 +0200

    setup tailwindcss

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,31 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: path.join(__dirname, "src", "index.js"),
     output: {
         path: path.resolve(__dirname, "dist"),
     },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     module: {
         rules: [{
-            test: /\.?js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react']
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
                 }
-            }
-        }, ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader", "postcss-loader",
+                ],
+            },
+        ]
     },
     performance: {
         maxEntrypointSize: 512000,
@@ -25,6 +34,10 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, "src", "index.html"),
+        }),
+        new MiniCssExtractPlugin({
+            filename: "styles.css",
+            chunkFilename: "styles.css"
         }),
     ],
 }
