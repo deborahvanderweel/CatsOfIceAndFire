e3d2b889064699c4b92b7111d29322a35c867565
deborahvanderweel <deborah.vanderweel@coolblue.nl>
2021-09-27 12:34:23 +0200

    Use react hooks instead of components

--- src/Components/CharacterList.jsx
+++ src/Components/CharacterList.jsx
@@ -1,34 +1,29 @@
 
 import Character from './Character';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default class CharacterList extends Component {
-    constructor() {
-        super();
-        this.state = {
-            data: []
-        };
-    }
+const CharacterList = () => {
+    const [data, setData] = useState(0);
 
-    componentDidMount() {
+    useEffect(() => {
         fetch("https://anapioficeandfire.com/api/characters?page=1&pageSize=100")
             .then(res => res.json())
-            .then(json => this.setState({ data: json }));
-    }
+            .then(json => setData({ data: json }));
+    })
 
-    render() {
-        const characters = this.state.data;
-        if (!characters || characters.length === 0) return <p>No characters, sorry</p>;
+    const characters = data;
+    if (!characters || characters.length === 0) return <p>No characters, sorry</p>;
 
-        return (
-            <div className="flex flex-wrap -mx-1 overflow-hidden sm:-mx-2 md:-mx-1 lg:-mx-2 font-mono">
-                <div className="container ml-auto mr-auto flex flex-wrap justify-center">
-                    <h1 className="text-5xl font-bold uppercase p-4">cats of ice and fire</h1>
-                </div>
-                <div className="container ml-auto mr-auto flex flex-wrap items-start">
-                    {characters.map(character => <Character {...character} key={character.id} />)}
-                </div>
+    return (
+        <div className="flex flex-wrap -mx-1 overflow-hidden sm:-mx-2 md:-mx-1 lg:-mx-2 font-mono">
+            <div className="container ml-auto mr-auto flex flex-wrap justify-center">
+                <h1 className="text-5xl font-bold uppercase p-4">cats of ice and fire</h1>
             </div>
-        );
-    };
+            <div className="container ml-auto mr-auto flex flex-wrap items-start">
+                {characters.data.map(character => <Character {...character} key={character.id} />)}
+            </div>
+        </div>
+    );
 }
+
+export default CharacterList;
--- src/Components/RandomImage.jsx
+++ src/Components/RandomImage.jsx
@@ -1,29 +1,25 @@
 
-import Character from './Character';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default class RandomImage extends Component {
-    constructor() {
-        super();
-        this.state = { data: [] };
-    }
+const RandomImage = () => {
+    const [data, setData] = useState(0);
 
-    componentDidMount() {
+    useEffect(() => {
         fetch("https://api.thecatapi.com/v1/images/search")
             .then(res => res.json())
-            .then(json => this.setState({ data: json }));
-    }
+            .then(json => setData({ data: json }));
+    })
 
-    render() {
-        const images = this.state.data;
-        if (!images || images.length === 0) return <p>No images, sorry</p>;
+    const images = data;
+    if (!images || images.length === 0) return <p>No images, sorry</p>;
 
-        return (
-            <div>
-                <figure className="mb-2">
-                    <img src={images[0].url} className="object-cover h-48 w-full rounded-t-xl" />
-                </figure>
-            </div>
-        );
-    };
+    return (
+        <div>
+            <figure className="mb-2">
+                <img src={images.data[0].url} className="object-cover h-48 w-full rounded-t-xl" />
+            </figure>
+        </div>
+    );
 }
+
+export default RandomImage;
